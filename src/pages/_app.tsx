24664be974483a7b1fc61bed683c73516cf1f338
Noat Eden <noat.eth@gmail.com>
2024-02-04 19:40:31 +0700

    add vercel speed insight

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import PageLayout from '@/components/PageLayout';
 import '@/styles/globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <PageLayout>
       <Component {...pageProps} />
+
+      <SpeedInsights />
     </PageLayout>
   );
 }
