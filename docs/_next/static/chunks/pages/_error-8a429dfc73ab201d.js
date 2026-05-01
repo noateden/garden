a2b599c69af9b5c54bb4633e39a0e91c0c2c7cab
Noat Eden <noat.eth@gmail.com>
2026-05-01 19:14:21 +0700

    use pnpm

---
+++ docs/_next/static/chunks/pages/_error-8a429dfc73ab201d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{5282:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(3094)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=5282)}),_N_E=n.O()}]);
